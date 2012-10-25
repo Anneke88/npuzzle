08af2d8233eabccbf759b1c9b6aed8272f0ce164
M.Emre Aydinli <hastelia@hotmail.com>
2012-10-25 14:06:42 -0500

    Adding main screen

--- N-puzzle/gen/com/challenges/mindsnacks/npuzzle/R.java
+++ N-puzzle/gen/com/challenges/mindsnacks/npuzzle/R.java
@@ -17,23 +17,37 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int mindsnacks=0x7f020002;
+        public static final int npuzzle=0x7f020002;
     }
     public static final class id {
-        public static final int activity_splash=0x7f080000;
-        public static final int logo=0x7f080001;
-        public static final int menu_settings=0x7f080002;
+        public static final int activity_splash=0x7f080005;
+        public static final int button_main_about=0x7f080003;
+        public static final int button_main_help=0x7f080002;
+        public static final int button_main_play=0x7f080000;
+        public static final int button_main_settings=0x7f080001;
+        public static final int imageView_logo=0x7f080004;
+        public static final int logo=0x7f080006;
+        public static final int menu_settings=0x7f080007;
     }
     public static final class layout {
-        public static final int activity_splash=0x7f030000;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_splash=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_splash=0x7f070000;
+        public static final int activity_main=0x7f070000;
+        public static final int activity_splash=0x7f070001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int btn_about=0x7f050008;
+        public static final int btn_help=0x7f050009;
+        public static final int btn_play=0x7f050006;
+        public static final int btn_settings=0x7f050007;
         public static final int hello_world=0x7f050001;
+        public static final int image_splashMindsnacks=0x7f050004;
+        public static final int img_main_logo=0x7f05000a;
         public static final int menu_settings=0x7f050002;
+        public static final int title_activity_main=0x7f050005;
         public static final int title_activity_splash=0x7f050003;
     }
     public static final class style {
--- N-puzzle/src/com/challenges/mindsnacks/npuzzle/SplashActivity.java
+++ N-puzzle/src/com/challenges/mindsnacks/npuzzle/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.challenges.mindsnacks.npuzzle;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.view.Menu;
 import android.view.Window;
@@ -11,25 +13,39 @@ import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 
 public class SplashActivity extends Activity {
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {		
-    	//Remove notification bar
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		//Remove title bar
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);		
-		startAnimations();
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_splash, menu);
-        return true;
-    }
-    
+	private final Runnable r_MainActivity = new Runnable() {
+		public void run() {
+			Intent myIntent = new Intent(SplashActivity.this,
+					MainActivity.class);
+			SplashActivity.this.startActivity(myIntent);
+			finish();
+		}
+	};
+
+	private final Handler h_getTo_nextActivity = new Handler();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// Remove notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		startAnimations();
+		
+		h_getTo_nextActivity.postDelayed(r_MainActivity, 4 * 1000);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_splash, menu);
+		return true;
+	}
+
 	private void startAnimations() {
 		Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
 		anim.reset();
